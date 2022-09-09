248c3a3f662cf5b72bd796b0aac5ec859f4f249f
Yiro Yi <yiroyi@live.com>
2022-09-10 00:39:15 +0900

    Setting default size of brush

--- app/src/main/java/com/example/kimchidrawing/DrawingView.kt
+++ app/src/main/java/com/example/kimchidrawing/DrawingView.kt
@@ -30,7 +30,6 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
       mDrawPaint!!.strokeJoin = Paint.Join.ROUND
       mDrawPaint!!.strokeCap = Paint.Cap.ROUND
       mCanvasPaint = Paint(Paint.DITHER_FLAG)
-      mBrushSize = 20.toFloat()
   }
 
   override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -94,6 +93,14 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
 
     invalidate()
     return true
+  }
+
+  fun setSizeForBrush(newSize : Float) {
+    mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+      newSize, resources.displayMetrics
+      )
+
+    mDrawPaint!!.strokeWidth = mBrushSize
   }
 
   internal inner class CustomPath(var color:Int,var brushThickness:Float):Path()
