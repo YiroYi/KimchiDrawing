36f9cc64d4c20ec491b90277623c367e51996de3
Yiro Yi <yiroyi@live.com>
2022-09-10 18:19:18 +0900

    Complete saving

--- app/src/main/java/com/example/kimchidrawing/MainActivity.kt
+++ app/src/main/java/com/example/kimchidrawing/MainActivity.kt
@@ -12,16 +12,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
-
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -66,7 +69,6 @@ class MainActivity : AppCompatActivity() {
       }
     }
 
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
@@ -82,7 +84,12 @@ class MainActivity : AppCompatActivity() {
 
     var ib_save : ImageButton = findViewById(R.id.ib_save)
     ib_save.setOnClickListener{
-
+      if(isReadStorageAllowed()) {
+        lifecycleScope.launch {
+          val flDrawingView: FrameLayout = findViewById(R.id.fl_drawing_view_container)
+          saveBitmapFile(getBitmapFromView(flDrawingView))
+        }
+      }
     }
 
     var ib_undo : ImageButton = findViewById(R.id.ib_undo)
@@ -145,6 +152,14 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  private fun isReadStorageAllowed(): Boolean {
+    val result = ContextCompat.checkSelfPermission(this,
+      Manifest.permission.READ_EXTERNAL_STORAGE
+      )
+
+    return result == PackageManager.PERMISSION_GRANTED
+  }
+
   private fun requestStoragePermission() {
     if(ActivityCompat.shouldShowRequestPermissionRationale(
         this,
@@ -155,15 +170,10 @@ class MainActivity : AppCompatActivity() {
         "needs to Access Your External Storage")
     } else {
       requestPermission.launch(arrayOf(
-        Manifest.permission.READ_EXTERNAL_STORAGE
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
       ))
     }
-  }
-
-  private fun isReadStorageAllowed() : Boolean {
-    val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-
-    return result == PackageManager.PERMISSION_GRANTED
   }
 
   private fun getBitmapFromView(view: View) : Bitmap {
@@ -180,6 +190,52 @@ class MainActivity : AppCompatActivity() {
     view.draw(canvas)
 
     return returnedBitmap
+  }
+
+  private suspend fun saveBitmapFile(mBitmap : Bitmap?): String {
+    var result = ""
+    withContext(Dispatchers.IO) {
+      if(mBitmap != null) {
+        try {
+          val bytes = ByteArrayOutputStream()
+          mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+          val f = File(externalCacheDir?.absoluteFile.toString() +
+            File.separator + "kimchidrawingApp_" + System.currentTimeMillis() / 1000 + ".png"
+          )
+
+          val fo = FileOutputStream(f)
+
+          fo.write(bytes.toByteArray())
+          fo.close()
+
+          result = f.absolutePath
+
+          runOnUiThread{
+            if(result.isNotEmpty()) {
+              Toast.makeText(
+                this@MainActivity,
+                "File saved succesfully: $result",
+                Toast.LENGTH_LONG
+
+              ).show()
+            }else {
+              Toast.makeText(
+                this@MainActivity,
+                "Soemthing when wrong",
+                Toast.LENGTH_LONG
+
+              ).show()
+            }
+          }
+        } catch(e: Exception) {
+          result = ""
+          e.printStackTrace()
+        }
+      }
+    }
+
+    return result
   }
 
   companion object {
